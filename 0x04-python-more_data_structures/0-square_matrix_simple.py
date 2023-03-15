495ef378af585ee9b057f63f134bd006dbc7b9d9
Benjamin Alvins Awino <77859328+benju99@users.noreply.github.com>
2023-03-15 22:03:08 +0300

    1. Search and replace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    return [list(map(lambda x: x ** 2, row)) for row in matrix]
+def search_replace(my_list, search, replace):
+    return [x if x != search else replace for x in my_list]
