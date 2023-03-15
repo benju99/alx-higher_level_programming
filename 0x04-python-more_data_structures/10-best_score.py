051e200a378520bdf6ffaef62e51ed1b776c3fa9
Benjamin Alvins Awino <77859328+benju99@users.noreply.github.com>
2023-03-15 23:44:09 +0300

    10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
+    best_key = None
+    best_score = 0
+    if not a_dictionary:
+        pass
+    else:
+        for key, value in a_dictionary.items():
+            if value > best_score:
+                best_score = value
+                best_key = key
+    return best_key
