c01f738866463c99e63c5659a6a24c42ad85eff7
Benjamin Alvins Awino <77859328+benju99@users.noreply.github.com>
2023-03-15 22:37:56 +0300

    2. Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    number = 0
-    for element in set(my_list):
-        number += element
-    return number
+    sum = 0
+    for i in set(my_list):
+        sum += i
+    return sum
